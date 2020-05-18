b3c482e3bde6f3e1ba06b0d030594c942d1914cd
Aleksei Timoshenko <alex.tim.03@gmail.com>
2020-05-18 19:30:20 +0300

    Добавление файлов приложения

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
+import Routes from 'routes'
+import TopBar from 'components/topBar'
+import CurrentUserChecker from 'components/currentUserChecker'
+import { CurrentUserProvider } from 'contexts/currentUser'
+
+const App = () => {
+  return (
+    <CurrentUserProvider>
+      <CurrentUserChecker>
+        <Router>
+          <TopBar />
+          <Routes />
+        </Router>
+      </CurrentUserChecker>
+    </CurrentUserProvider>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
